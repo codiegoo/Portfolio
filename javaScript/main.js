532f7866c459b0b8fb80298d19f8d851305fabd1
DIEGO FLORES ALCANTAR <diegoedvflores03@gmail.com>
2022-10-23 18:49:40 -0500

    Conectando Repositorio

---
+++ javaScript/main.js
@@ -0,0 +1,13 @@
+const typed = new Typed('.typed', {
+    strings: [
+        '<i class="wordRed">Frontend!</i>',
+        '<i class="wordRed">Backend!</i>'],
+    typeSpeed: 150,
+    startDelay: 500,
+    backSpeed: 100,
+    shuffle: false,
+    backDelay: 2000,
+    loop: true,
+    loopCount: false,
+    cursorChar: '|👾',
+});
